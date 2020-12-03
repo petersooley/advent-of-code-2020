d487c22e6654749ad6cf9a5017b45a95b5598c64
Peter Sooley <peter.sooley@gmail.com>
2020-12-02 21:01:29 -0500

    day2 done, vec::fold to the rescue

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,18 +1,17 @@
+use regex::{Captures, Regex};
 use std::env;
 use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::str::FromStr;
-use regex::{Regex, Captures};
-
 
 fn split_line(s: &str) -> Result<Captures, String> {
     let re = Regex::new(r"(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)")
-    .map_err(|_| String::from("failed to create regex"))?;
+        .map_err(|_| String::from("failed to create regex"))?;
 
     let caps = re
-    .captures(s)
-    .ok_or_else(|| format!("regex failed to find captures any line: '{}'", s))?;
+        .captures(s)
+        .ok_or_else(|| format!("regex failed to find captures any line: '{}'", s))?;
 
     if caps.len() == 0 {
         return Err(format!("not enough captures found in line: '{}'", s));
@@ -22,36 +21,56 @@ fn split_line(s: &str) -> Result<Captures, String> {
 }
 
 #[derive(Debug)]
-struct PasswordChecker {
-    min: u8,
-    max: u8,
+struct PasswordEntry {
+    left: usize,
+    right: usize,
     target: char,
     password: String,
 }
 
-impl FromStr for PasswordChecker {
+impl PasswordEntry {
+    fn target_count(&self) -> usize {
+        self.password.matches(self.target).count()
+    }
+
+    fn left_char(&self) -> Option<char> {
+        self.password.chars().nth(self.left - 1)
+    }
+
+    fn right_char(&self) -> Option<char> {
+        self.password.chars().nth(self.right - 1)
+    }
+}
+
+impl FromStr for PasswordEntry {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // lazy_static! {
-        //     static ref re: Regex::new(r"(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)")
-        //           .map_err(|_| String::from("failed to create regex"))?;
-        // }
         let caps = split_line(s)?;
 
-        let min = caps
+        let left = caps
             .get(1)
-            .ok_or_else(|| format!("no min value found in line: '{}'", s))?
+            .ok_or_else(|| format!("no left value found in line: '{}'", s))?
             .as_str()
-            .parse::<u8>()
-            .map_err(|e| format!("failed to parse u8 min from line: '{}'. error: {}", s, e))?;
+            .parse::<usize>()
+            .map_err(|e| {
+                format!(
+                    "failed to parse usize left from line: '{}'. error: {}",
+                    s, e
+                )
+            })?;
 
-        let max = caps
+        let right = caps
             .get(2)
-            .ok_or_else(|| format!("no max value found in line: '{}'", s))?
+            .ok_or_else(|| format!("no right value found in line: '{}'", s))?
             .as_str()
-            .parse::<u8>()
-            .map_err(|e| format!("failed to parse u8 max from line: '{}'. error: {}", s, e))?;
+            .parse::<usize>()
+            .map_err(|e| {
+                format!(
+                    "failed to parse usize right from line: '{}'. error: {}",
+                    s, e
+                )
+            })?;
 
         let target = caps
             .get(3)
@@ -67,27 +86,30 @@ impl FromStr for PasswordChecker {
             .as_str()
             .to_string();
 
-        Ok(PasswordChecker {
-            min,
-            max,
+        Ok(PasswordEntry {
+            left,
+            right,
             target,
             password,
         })
     }
 }
 
-impl PasswordChecker {
-    fn is_valid(&self) -> Option<()> {
-        let count = self.target_count();
-        if count > self.max || count < self.min {
-            return None;
-        }
-        Some(())
+fn by_count(entry: &PasswordEntry) -> Option<()> {
+    let count = entry.target_count();
+    if count > entry.right || count < entry.left {
+        return None;
     }
+    Some(())
+}
 
-    fn target_count(&self) -> u8 {
-        self.password.matches(self.target).count() as u8
+fn by_index(entry: &PasswordEntry) -> Option<()> {
+    let l = entry.left_char().expect("invalid left index");
+    let r = entry.right_char().expect("invalid right index");
+    if l != r && (l == entry.target || r == entry.target) {
+        return Some(());
     }
+    return None;
 }
 
 fn main() -> io::Result<()> {
@@ -99,17 +121,22 @@ fn main() -> io::Result<()> {
     let f = fs::File::open(&args[1])?;
     let reader = io::BufReader::new(f);
 
-    let valid_count = reader
+    let results = reader
         .lines()
-        .filter_map(|l| -> Option<()> {
-            l.expect("failed to read line")
-                .parse::<PasswordChecker>()
-                .unwrap()
-                .is_valid()
-        })
-        .count();
+        .fold((0 as u64, 0 as u64), |acc, l| -> (u64, u64) {
+            let entry = l
+                .expect("failed to read line")
+                .parse::<PasswordEntry>()
+                .unwrap();
 
-    println!("{}", valid_count);
+            (
+                acc.0 + by_count(&entry).map_or(0, |_| 1),
+                acc.1 + by_index(&entry).map_or(0, |_| 1),
+            )
+        });
+
+    println!("{}", results.0);
+    println!("{}", results.1);
 
     Ok(())
 }
